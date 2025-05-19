7533b3998cb87652f029a98ecc6b36dc0e18fd43
NonstopCola <105921038@student.swin.edu.au>
2025-05-19 19:59:39 +1000

    Minor changes to the enhancements.php file

--- enhancements.php
+++ enhancements.php
@@ -13,7 +13,7 @@
             <img id="Logo" src="./images/final_logo.png" alt="JKTN Logo">
     <!--company logo created through chatgpt.com using the prompt "Create a logo for a tech company called JKTN using a simplistic design and the base colour blue" -->
         </div>
-        <h1>Enhancements</h1>
+        <h1>Enhancements Page</h1>
         <?php
             // Sets the active page for navigation highlighting
             $activePage = 'enhancements';
@@ -22,7 +22,13 @@
             // Redirects to error page if an issue arises, sets it so a login is required
             $require_login = true;
             include './redirect.inc';
-            
+        ?>
+        <h2 class='setMiddle'>Enhancements</h2>
+        <section id="manageEnhancements">
+            <h3 class='setMiddle'>Management</h3>
+        
+        </section>
+        <?php
             // Sets up the footer
             include './footer.inc';
         ?>
