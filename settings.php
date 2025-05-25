7ddfb9b8d89b49c58fd5feb2634ad27f4cb124ee
NonstopCola <105921038@student.swin.edu.au>
2025-05-25 17:09:14 +1000

    A database is now created if it doesn't already exist.

--- settings.php
+++ settings.php
@@ -4,9 +4,18 @@ $username = "root";
 $password = "";
 $database = "jktn";
 
-$conn = mysqli_connect($host, $username, $password, $database);
-
+// Connect without specifying a database so we can run the CREATE DATABASE command
+$conn = mysqli_connect($host, $username, $password);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+// Create the database if it doesn't exist
+$createDBQuery = "CREATE DATABASE IF NOT EXISTS $database";
+if (!mysqli_query($conn, $createDBQuery)) {
+    die("Database creation failed: " . mysqli_error($conn));
+}
+
+// Now select the database
+mysqli_select_db($conn, $database);
 ?>
